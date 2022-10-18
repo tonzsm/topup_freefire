331a799da411e18648c922ec794f69092d3e1ec1
swmkook <49269612+swmkook@users.noreply.github.com>
2022-10-18 20:13:17 +0700

    Update index.php
    update data

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ include 'uidfreefire.class.php';
 //key antirecaptcha ไปหาซื้อกันเองนะครัชชช
 $token_antirecaptcha = "";
 $topup = New topup_freefire($token_antirecaptcha);
-$uid = '950516437';
+$uid = ''; //uid platform
 //login ด้วย uid freefire ก่อน
 print_r($topup->login_freefire($uid));
 //login เสร็จแล้วจะได้open_idไว้เติมฟรีฟายด้วยบัตรการีน่า
-$open_id = '15e3bee77f5460d82b6000ed9f470d99';
-$garenacard = '9811329697982418';
+$open_id = ''; //open id after login
+$garenacard = ''; //garena card
 print_r($topup->topup_garenacard($open_id,$garenacard));
